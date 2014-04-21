c54589453d64adcd44a1a766f92c50c7ac7a6708
U-FINTECH\fedotov <fedotov@fedotov.fintech.ru>
2014-04-21 14:04:53 +0400

    added the root index for encoding test

---
+++ bytemag/rss/index.php
@@ -0,0 +1,191 @@
+<?xml version="1.0" encoding="Windows-1251"?><rss version="2.0" >
+<channel>
+<title>BYTE/Россия</title>
+<link>http://www.bytemag.ru</link>
+<description>Журнал BYTE/Россия охватывает широкий диапазон вопросов, в числе которых: информационная безопасность, программное и аппаратное обеспечение, архитектура информационных систем, управление данными, разработка программного обеспечения для различных операционных платформ и многое другое.</description>
+<lastBuildDate>Mon, 21 Apr 2014 13:38:16 +0400</lastBuildDate>
+<ttl>24</ttl>
+<image>
+	<title>BYTE/Россия</title>
+	<url>http://www.bytemag.ru/images/byte/logo-rss.gif</url>
+	<link>http://www.bytemag.ru</link>
+	<width>100</width>
+	<height>56</height>
+</image>
+<item>
+	<title>Год на российском рынке ИБП оказался очень сложным</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25286</link>
+	<description>По данным аналитической компании ITResearch в 2013 г. в России было продано 1,95 млн ИБП на сумму 503,6 млн долл. Годовая динамика составила &amp;ndash;17% в &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Sat, 19 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Печатная продукция становится интерактивной</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25287</link>
+	<description>Способ маркировки товаров обычными линейными штрихкодами уже привычен пользователям. Штрихкод на упаковке давно не вызывает вопросов &amp;laquo;Зачем это &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Sat, 19 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Устройства Lenovo c защитой «АльтЭль»</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25288</link>
+	<description>Компании &amp;laquo;АльтЭль&amp;raquo;, российский разработчик средств защиты информации, и Lenovo объявили о завершении работ по интеграции модуля доверенной загрузки &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Sat, 19 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Облачные решения Schneider Electric и Microsoft: управление энергоэффективностью и экологичностью</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25289</link>
+	<description>Как объявила компания Schneider Electric, в рамках ее альянса с Microsoft теперь появилась возможность разрабатывать облачные решения для управления &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Sat, 19 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Брандмауэр в Dr.Web для Android</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25282</link>
+	<description>Компания &amp;laquo;Доктор Web&amp;raquo; сообщает о выпуске Dr.Web версии 9.01.0 для Android, в которую включен новый компонент &amp;ndash; брандмауэр для блокировки &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Fri, 18 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Портал DeskWork, новая редакция </title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25283</link>
+	<description>Компания &amp;laquo;Дэскворк&amp;raquo; выпустила обновленную версию своего корпоративного портала &amp;ndash; DeskWork Q1 2014. В новой редакции расширен функционал и внесен ряд &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Fri, 18 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Высокоскоростной интерфейс в графических процессорах NVIDIA</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25284</link>
+	<description>Компания NVIDIA объявила о намерении интегрировать высокоскоростной интерфейс NVLink в будущие графические процессоры, что должно ускорить передачу &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Fri, 18 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>МФУ Brother Ink Benefit с картриджами повышенной емкости</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25285</link>
+	<description>Компания Brother выпустила новую линейку струйных многофункциональных устройств Ink Benefit, обеспечивающих низкую стоимость печати за счет &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Fri, 18 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Ноутбуки HP в «новом стиле»</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25279</link>
+	<description>Компания HP представила новые модели ноутбуков и рабочих станций, созданных в рамках концепции &amp;laquo;Нового стиля IT&amp;raquo;. Среди новинок &amp;ndash; HP Pavilion x360 &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Thu, 17 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Многокристальные устройства от Intel и Altera</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25280</link>
+	<description>Компании Intel и Altera объявили о сотрудничестве в области разработки многокристальных устройств, которые будут создаваться на базе производственных &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Thu, 17 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Хромбукам предрекают быстрый рост</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25281</link>
+	<description>Согласно данным исследовательской фирмы ABI Research, в 2013 г. в мире было поставлено 2,1 млн хромбуков (Chromebooks), из них около 89% было &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Thu, 17 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>DaaS-платформа для работы с графикой в облаке </title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25256</link>
+	<description>Компании NVIDIA и VMware объявили о доступности технологии NVIDIA Grid в рамках платформы VMware Horizon DaaS Platform, чтобы обеспечить поддержку &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Wed, 16 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Недорогая беспроводная точка доступа Cisco </title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25257</link>
+	<description>Компания Cisco пополнила линейку точек беспроводного доступа новым недорогим устройством Cisco Aironet 2700 с поддержкой стандарта Wi-Fi следующего &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Wed, 16 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Autodesk потеряла одного из крупнейших партнеров в России</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25259</link>
+	<description>Компания &amp;laquo;СиСофт&amp;raquo;, один из крупнейших в России поставщиков решений Autodesk, приняла решение о расторжении партнерских отношений с американской &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Wed, 16 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Новые угрозы атак на пользователей онлайн-банкинга </title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25260</link>
+	<description>Компания eScan сообщила об угрозе почтовых фишинговых атак, в ходе которых пользователям онлайновых банковских услуг предлагают обновить свои &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Wed, 16 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Сетевые новинки Dell для дата-центров </title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25261</link>
+	<description>Компания Dell представила новые сетевые решения &amp;ndash; коммутатор фиксированного форм-фактора Dell Networking Z9500 с высокой плотностью портов и &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Wed, 16 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>В сегменте панелей для видеостен конкурентная борьба идет в технологической сфере</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25262</link>
+	<description>На рынке информационных панелей, если судить по отчетам аналитиков, растет потенциал и улучшаются перспективы сегмента дисплеев для видеостен. Этот &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Wed, 16 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Отраслевое решение Oracle Utilities Analytics 2.5 для энергетики и ЖКХ</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25242</link>
+	<description>Корпорация Oracle анонсировала начало продаж решения Oracle Utilities Analytics 2.5 с расширенными возможностями встроенных информационных панелей &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Tue, 15 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Еще 18 госуслуг – в электронном виде </title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25243</link>
+	<description>На Едином портале государственных услуг (ЕПГУ) появились 18 новых услуг, доступных для получения в электронном виде. К ним относятся государственные &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Tue, 15 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Облачная тенденция внесла коррективы в рейтинг производителей ПО</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25244</link>
+	<description>Согласно исследованиям аналитической фирмы Gartner, мировой рынок ПО в 2013 г. увеличился на 4,8% &amp;ndash; до 407,3 млрд долл., тогда как в 2012 г. его &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Tue, 15 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Компактный моноблок HP для малого и среднего бизнеса  </title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25245</link>
+	<description>Корпорация НР анонсировала стильные, надежные и доступные по цене моноблоки (AiO) на платформе Windows 8.1, расширяющие ее линейку решений для малого &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Tue, 15 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>Углубленная аналитика от Qlik и Alteryx</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=25255</link>
+	<description>Компании Alteryx, разработчик решений для комбинирования данных и углубленной аналитики, и Qlik, специализирующаяся на системах бизнес-анализа для &amp;hellip;</description>
+				<category>2014/</category>
+			<pubDate>Tue, 15 Apr 2014 00:00:00 +0400</pubDate>
+</item>
+<item>
+	<title>«Школа сисадмина» Digital Design </title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=14095</link>
+	<description>В Центре технического обучения компании Digital Design состоялось официальное открытие образовательного проекта «Школа Сисадмина». Школа …</description>
+				<category>2009/</category>
+			<pubDate>Mon, 21 Apr 2014 13:38:16 +0400</pubDate>
+</item>
+<item>
+	<title>Entensys патентует модуль UserGate Psychoanalytics</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=14277</link>
+	<description>Компания Entensys объявила о подаче заявки на патент и внедрении технологии психоэмоционального анализа состояния человека в прокси-сервере UserGate …</description>
+				<category>2009/</category>
+			<pubDate>Mon, 21 Apr 2014 13:38:16 +0400</pubDate>
+</item>
+<item>
+	<title>Услуга «Антивирус Dr.Web» в Екатеринбурге</title>
+	<link>http://www.bytemag.ru/articles/detail.php?ID=14538</link>
+	<description>Как сообщила компания &amp;laquo;Доктор Веб&amp;raquo;, еще один филиал компании &amp;laquo;СТРИМ-ТВ&amp;raquo;, оператора платного телевидения и широкополосного доступа в Интернет, внедрил &amp;hellip;</description>
+				<category>2009/</category>
+			<pubDate>Mon, 21 Apr 2014 13:38:16 +0400</pubDate>
+</item>
+</channel>
+</rss>
